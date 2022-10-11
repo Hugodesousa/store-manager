0bca6ecdac6de69665122c5f4af493095523ae2e
Hugodesousa <hugodesousa.16@gmail.com>
2022-10-11 17:12:08 -0300

    initial conection

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const connection = require('./models/db/connection');
 
 const app = express();
 
@@ -7,7 +8,19 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
+const port = 9229;
+
+app.listen(port, async () => {
+  console.log(`API está sendo executada na porta ${port}`);
+
+  // O código abaixo é para testarmos a comunicação com o MySQL
+  const [result] = await connection.execute('SELECT 1');
+  if (result) {
+    console.log('MySQL connection OK');
+  }
+});
+
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
-module.exports = app;
+  module.exports = app;
