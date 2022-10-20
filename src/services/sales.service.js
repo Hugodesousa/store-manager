400456abd0b7a39c9c92245dc2e4e126fd0c9969
Hugodesousa <hugodesousa.16@gmail.com>
2022-10-20 09:57:49 -0300

    Requisito 06

--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -1,29 +1,27 @@
 const { salesModel, productsModel } = require('../models');
 const { validCreatSale } = require('./utils/validationCreatSales');
 
-const errorNotID = async (id) => {
-  const test = await productsModel.findProductsByID(id);
-  return test;
+const checkValidID = async (salesList) => { 
+  const findProduct = salesList.map((sale) =>
+    productsModel.findProductsByID(sale.productId));
+  const resultProduct = await Promise.all(findProduct);
+  console.log('result', resultProduct);
+
+  const checkResult = resultProduct.some((value) => typeof value !== 'object');
+  console.log('check', checkResult);
+  if (checkResult) {
+    const error = new Error('Product not found');
+    error.name = 'Product not found';
+    error.status = 404;
+    throw error;
+  }
+  return true;
 };
 
-const creatSales = async (salesList) => { 
-  console.log(validCreatSale(salesList));
-  for (let index = 0; salesList.length > index; index += 1) {
-    // const test = 
-    const test = await errorNotID(salesList[index].productId);
-    console.log('test', test);
-    const error = new Error();
-    if (!test) {
-      error.message = 'Product not found';
-      error.name = 'Product not found';
-      error.status = 404;
-      throw error;
-    }
-  }
-
-  // salesList.map(async (sale) => {
-  //   const test = await productsModel.findProductsByID(sale.productId);
-  //   console.log('test', test);
+const creatSales = async (salesList) => {
+  // for (let index = 0; salesList.length > index; index += 1) {
+  //   // const test =
+  //   const test = await productsModel.findProductsByID(salesList[index].productId);
   //   const error = new Error();
   //   if (!test) {
   //     error.message = 'Product not found';
@@ -31,9 +29,11 @@ const creatSales = async (salesList) => {
   //     error.status = 404;
   //     throw error;
   //   }
-  // });
+  // }
+
   if (validCreatSale(salesList)) {
     // console.log('aqui', validCreatSale(salesList));
+    await checkValidID(salesList);
     const result = await salesModel.insertSale(salesList);
     return result;
   }
--- src/services/utils/validationCreatSales.js
+++ src/services/utils/validationCreatSales.js
@@ -22,21 +22,9 @@ const parameterNotFound = (key, message) => {
   }
 };
 
-// const findID = async (id) => { 
-//   const test = await productsModel.findProductsByID(toString(id));
-//   if (!test) {
-//     const error = new Error('Product not found');
-//     error.name = 'Product not found';
-//     error.status = 422;
-//     throw error;
-//   }
-// };
 const validCreatSale = (list) => {
-const validate = list.map((sale) => {
-  // findID(sale.productId);
+  list.map((sale) => {
   const { error } = parameterIsRequired.validate(sale);
-  // const ids.data = 
-  // console.log(sale.productId);
     if (error !== undefined) {
       const { message, context } = error.details[0];
       
@@ -45,36 +33,8 @@ const validate = list.map((sale) => {
 
     return true;
 });
-  return validate;
+  return true;
 };
-
-// list.forEach((element) => {
-//   if (!element.productId) { 
-//     const error = new Error('"productId" is required');
-//     error.name = 'product ID not found';
-//     error.status = 400;
-//     throw error;
-//   }
-//   if (!element.quantity) {
-//     const error = new Error('"quantity" is required');
-//     error.name = 'quantity not found';
-//     error.status = 400;
-//     throw error;
-//   }
-//   if (element.quantity <= 0) {
-//     const error = new Error('"quantity" must be greater than or equal to 1');
-//     error.name = 'quantity small';
-//     error.status = 422;
-//     throw error;
-//   } 
-//   if (element.productId <= 0) {
-//     const error = new Error('"quantity" must be greater than or equal to 1');
-//     error.name = 'quantity small';
-//     error.status = 422;
-//     throw error;
-//   }
-// });
-// return true;
 
 module.exports = {
   validCreatSale,
